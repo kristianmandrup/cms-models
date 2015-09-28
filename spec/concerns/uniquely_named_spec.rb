fbb22b2cd9f545b990c6672a20f4f0756527e9e1
Ankush K <katariyaankush574@gmail.com>
2015-09-28 13:50:01 +0530

    add specs for uniquely named

--- spec/concerns/uniquely_named_spec.rb
+++ spec/concerns/uniquely_named_spec.rb
@@ -5,5 +5,21 @@ class UniquelyNamedModel < Cms::Models::Document
 end
 
 describe UniquelyNamed do
-  pending 'todo'
+  let(:publishable_model) { UniquelyNamedModel.create name: 'test name' }
+  
+  it "should present a name field" do
+    expect(UniquelyNamedModel.fields.keys).to include('name')
+  end
+  
+  it "should not have empty name" do
+    publishable_model.name = nil
+    expect(publishable_model).to be_invalid
+  end
+  
+  it "should have unique name" do
+    publishable_model
+    publishable_model1 = UniquelyNamedModel.create name: 'test name'
+    expect(publishable_model1).to be_invalid
+  end
+  
 end
