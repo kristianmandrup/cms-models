9aa07cb97b956c304cdb3763e03d7e3e93b180d0
Ankush K <katariyaankush574@gmail.com>
2015-09-30 00:00:18 +0530

    added specs for uploadable and mock with vcr

--- spec/concerns/uploadable_spec.rb
+++ spec/concerns/uploadable_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
+
 class UploadableModel < Cms::Models::Document
   include Uploadable
-  
+
 end
 
 describe Uploadable do
-  pending 'todo'
+  it "should upload image" do
+    VCR.use_cassette('image_upload', :match_requests_on => [:method, :host]) do
+      file = File.new(File.join('spec', 'fixtures', 'ruby.png'), 'r')
+      uploadable_model = UploadableModel.create img_content: file
+      url = URI(uploadable_model.img_content.url)
+      expect(url.host).to eq('tecla-cms.s3-eu-central-1.amazonaws.com')
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,16 @@ VCR.configure do |c|
 end  
 
 
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+      :provider               => 'aws',
+      :aws_access_key_id      => ENV['AWS_KEY'],
+      :aws_secret_access_key  => ENV['AWS_SECRET'],
+      :region                 => ENV['AWS_REGION'] # Change this for different AWS region. Default is 'us-east-1'
+  }
+  config.fog_directory  = ENV['AWS_BUCKET']
+end
+  
 CarrierWave::Backgrounder.configure do |c|
   c.backend :resque, queue: :carrierwave
 end
