d847a9d93c07ca599ed6dc36f8a7f6490e403f7d
Ankush K <katariyaankush574@gmail.com>
2015-09-30 21:08:42 +0530

    added extendable specs

--- lib/cms/models/attribute/generic.rb
+++ lib/cms/models/attribute/generic.rb
@@ -2,11 +2,7 @@ module Cms
   module Models
     module Attribute
       class Generic < Cms::Models::Document
-        include Mongoid::Document
-        include Mongoid::Timestamps
-          
         field :caption, type: String
-        field :type, type: String
         field :filters, type: String
         field :input, type: String
         field :output, type: String
--- spec/concerns/extendable_spec.rb
+++ spec/concerns/extendable_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 class ExtendableModel < Cms::Models::Document
   include Extendable
+  
   field :name, type: String
 end
 
@@ -13,5 +14,22 @@ describe Extendable do
     expect(extendable_model).to be_valid
   end
   
-  it "should have genric attributes"
+  it "should have genric attributes" do
+    extendable_model = ExtendableModel.create(
+                                      name: "test", 
+                                      generic_attributes_attributes: 
+                                      [
+                                          {caption: 'caption1', input: 'input1', output: 'output1', value: 'value1' },
+                                          {caption: 'caption2', input: 'input2', output: 'output2', value: 'value2' }
+                                      ]
+                        )
+    generic_attributes = extendable_model.generic_attributes
+    expect(generic_attributes.count).to eq(2)
+    
+    expect(generic_attributes[0].caption).to eq('caption1')
+    expect(generic_attributes[0].generic_type).to eq('ExtendableModel')
+    
+    expect(generic_attributes[1].caption).to eq('caption2')
+    expect(generic_attributes[1].generic_type).to eq('ExtendableModel')
+  end
 end
