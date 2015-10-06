be731ada2cc103c96930dd530d6539afcada986c
Ankush K <katariyaankush574@gmail.com>
2015-10-06 20:35:29 +0530

    added specs into content item

--- spec/concerns/content_item_spec.rb
+++ spec/concerns/content_item_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 require 'concerns/uniquely_named_spec'
 require 'concerns/meta_info_spec'
 require 'concerns/translatable_spec'
@@ -6,3 +8,31 @@ require 'concerns/renderable_spec'
 require 'concerns/publishable_spec'
 require 'concerns/blueprintable_spec'
 require 'concerns/serializable_spec'
+
+class ContentItemModel < Cms::Models::Document
+  include ContentItem
+  
+  metadata_fields keywords: Array, author: String
+end
+
+RSpec.shared_examples "ContentItem" do
+  
+  it_behaves_like "UniquelyNamed" do
+    let(:uniquely_named_model) { content_item_model }
+  end
+  
+  it_behaves_like "MetaInfo" do
+    let(:meta_info_model) { content_item_model }
+  end  
+
+end
+
+RSpec.describe ContentItemModel do
+  it_behaves_like "ContentItem" do
+    let(:categories) { Faker::Lorem.words(2) }
+    let(:tags) { Faker::Lorem.words(2) }
+    let(:description) { Faker::Lorem.sentence }
+    let(:content_item_model) { ContentItemModel.create name:  Faker::Lorem.characters(10), description: description, categories: categories, tags: tags }
+  end
+end
+
