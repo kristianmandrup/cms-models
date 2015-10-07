d858df6a9dfb835fe90d6da6b307d4f55769467f
Ankush K <katariyaankush574@gmail.com>
2015-10-07 21:08:08 +0530

    make reusable tags array

--- spec/concerns/taggable_spec.rb
+++ spec/concerns/taggable_spec.rb
@@ -11,12 +11,13 @@ RSpec.shared_examples "Taggable" do
   end
   
   it "should store array of tag" do
-    expect(taggable_model.tags).to eq(['tag1','tag2','tag3'])
+    expect(taggable_model.tags).to eq(tags)
   end
 end
 
 RSpec.describe TaggableModel do
   it_behaves_like "Taggable" do
-    let(:taggable_model) { TaggableModel.create tags: ['tag1','tag2','tag3'] }
+    let(:tags) { Faker::Lorem.words(4) }
+    let(:taggable_model) { TaggableModel.create tags: tags }
   end
 end
