348a1cfd2ee795bbb0bb37f66a303eb056c96df0
Ankush K <katariyaankush574@gmail.com>
2015-10-12 13:18:10 +0530

    write specs for template model

---
+++ spec/models/template_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'concerns/uniquely_named_spec'
+RSpec.describe Cms::Models::Template, type: :model do
+
+  let(:template) {FactoryGirl.create(:template)}
+  
+  def field_names
+    Cms::Models::Template.fields.keys
+  end
+  
+  it_behaves_like "UniquelyNamed" do
+    let(:name) { Faker::Lorem.characters(10) }
+    let(:uniquely_named_model) { FactoryGirl.create(:template, name: name) }
+  end
+
+  it 'should have valid template factory' do
+    expect(template).to be_valid
+  end
+  
+  it 'should have format field with default value html' do
+    expect(template.format).to eq('html')
+  end
+  
+  it 'should have handler field with default value liquid' do
+    expect(template.handler).to eq('liquid')
+  end
+  
+  it 'should have type field with default value template' do
+    expect(template.type).to eq('template')
+  end
+  
+  it 'should have template field' do
+    expect(field_names).to include('template')
+  end
+  
+end
